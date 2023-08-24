680f150b9798e3b6cc45116f64955f5d35720a3b
MoozIiSP <9022638+MoozIiSP@users.noreply.github.com>
2023-08-24 13:06:24 +0800

    u

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -29,9 +29,6 @@
   '';
 
   users = {
-    # Using a purely declarative user setup. This means any future users will have to
-    # have a password hash generated with `mkpasswd`
-    #mutableUsers = false;
     # security
     # Needs to be set explicitly because nixos-wsl disables this for ease of installation
     #security.sudo.wheelNeedsPassword = true;
@@ -46,10 +43,16 @@
         hashedPassword = "$6$MzkWefhNrnurSwZV$WE7fMyun5BBnJ611F8zRnkyvIsQ7nl8BCszOcOg4Saw7f7ESoXsMszjfUFD4Nr.VijSKP/yKytVO2Ptl7WIFb/";
       };
       ftp = {
+        isNormalUser = true;
+        group = "ftp";
         hashedPassword = "$6$qqLx5clVPMJtNVbl$rIt87Xn2Igjr20B3sq3iL61ER8j1WzSa2U3IcG5qpcjAOaObW4o1e3dKEpEwNm8pudL5lKt2/3be4qdUPQ8iY1";
       };
     };
   };
+
+  # Using a purely declarative user setup. This means any future users will have to
+  # have a password hash generated with `mkpasswd`
+  users.mutableUsers = false;
 
   networking.hostName = "bocft-wsl";
 
