a011da288f5beefd64c34ab5f21604b03dee9117
MoozIiSP <9022638+MoozIiSP@users.noreply.github.com>
2023-08-24 02:53:39 +0000

    u

--- build-wsl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd workspace
-nix build ./workspace/.#nixosConfigurations.wsl.config.system.build.installer --extra-experimental-features nix-command --extra-experimental-features flakes
-cd -
